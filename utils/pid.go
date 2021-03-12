38bba5b90d7670496ce4d8e69f430f822254351d
packing <563388@qq.com>
2021-03-13 01:39:16 +0800

    Ignore compiling daemons on Windows.

--- utils/pid.go
+++ utils/pid.go
@@ -18,69 +18,69 @@
 package utils
 
 import (
-	"fmt"
-	"os"
-	"strings"
-	"strconv"
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
 )
 
 func GeneratePID(pidFile string) {
-	pf, err := os.OpenFile(pidFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0664)
-	if err != nil {
-		LogError("Write pidfile error!", err)
-		return
-	}
+    pf, err := os.OpenFile(pidFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0664)
+    if err != nil {
+        LogError("Write pidfile error!", err)
+        return
+    }
 
-	pf.WriteString(fmt.Sprintf("%d\n", os.Getpid()))
-	pf.Close()
+    pf.WriteString(fmt.Sprintf("%d\n", os.Getpid()))
+    pf.Close()
 }
 
 func RemovePID(pidFile string) {
-	err, pids := ReadPIDs(pidFile)
-	if err != nil {
-		LogError("Remove from pidfile error!", err)
-		return
-	}
+    err, pids := ReadPIDs(pidFile)
+    if err != nil {
+        LogError("Remove from pidfile error!", err)
+        return
+    }
 
-	pf, err := os.OpenFile(pidFile, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0664)
-	if err != nil {
-		LogError("Remove from pidfile error!", err)
-		return
-	}
+    pf, err := os.OpenFile(pidFile, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0664)
+    if err != nil {
+        LogError("Remove from pidfile error!", err)
+        return
+    }
 
-	for _,pid := range pids {
-		if pid == os.Getpid() {
-			continue
-		}
-		pf.WriteString(fmt.Sprintf("%d\n", pid))
-	}
-	pf.Close()
+    for _, pid := range pids {
+        if pid == os.Getpid() {
+            continue
+        }
+        pf.WriteString(fmt.Sprintf("%d\n", pid))
+    }
+    pf.Close()
 }
 
 func ReadPIDs(pidFile string) (error, []int) {
-	pf, err := os.Open(pidFile)
-	if err != nil {
-		LogError("read from pidfile error!", err)
-		return err, nil
-	}
+    pf, err := os.Open(pidFile)
+    if err != nil {
+        LogError("read from pidfile error!", err)
+        return err, nil
+    }
 
-	bs := make([]byte, 10240)
-	n, err := pf.Read(bs)
-	pf.Close()
-	if err != nil {
-		LogError("read from pidfile error!", err)
-		return err, nil
-	}
-	ctx := string(bs[:n])
-	pids := strings.Split(ctx, "\n")
-	intpids := make([]int, len(pids))
-	i := 0
-	for _, v := range pids {
-		nv, err := strconv.Atoi(v)
-		if err == nil {
-			intpids[i] = nv
-			i ++
-		}
-	}
-	return nil, intpids[:i]
+    bs := make([]byte, 10240)
+    n, err := pf.Read(bs)
+    pf.Close()
+    if err != nil {
+        LogError("read from pidfile error!", err)
+        return err, nil
+    }
+    ctx := string(bs[:n])
+    pids := strings.Split(ctx, "\n")
+    intpids := make([]int, len(pids))
+    i := 0
+    for _, v := range pids {
+        nv, err := strconv.Atoi(v)
+        if err == nil {
+            intpids[i] = nv
+            i++
+        }
+    }
+    return nil, intpids[:i]
 }
